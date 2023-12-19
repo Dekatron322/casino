6419645da771c7c1b9c35205d39bc0e1908b1a9c
Dekatron322 <muritalaibrahim097@gmail.com>
2023-12-20 00:46:52 +0100

    gamba init

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
+
+export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
+          <NavLink className={styles.logo} to="/">
+            <img alt="Gamba logo" src="https://poemonk.com/images/0poem.png" />
+            <span>Peom Casino</span>
+          </NavLink>
+        </div>
+        <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
+          <div className={styles.externalLinks}>
+            <a
+              href="https://github.com/gamba-labs/gamba"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://explorer.gamba.so"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Stats
+            </a>
+          </div>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
