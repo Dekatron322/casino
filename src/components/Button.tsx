56b1133a1bb455cf520f5f0208a42307347ad123
Dekatron322 <muritalaibrahim097@gmail.com>
2023-12-20 14:11:51 +0100

    connect

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,61 +1,59 @@
-import React from 'react'
-import { cx } from '../utils'
-import styles from './Button.module.css'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { cx } from "../utils";
+import styles from "./Button.module.css";
+import { useNavigate } from "react-router-dom";
 
-type ButtonSize = 'small' | 'medium' | 'large'
+type ButtonSize = "small" | "medium" | "large";
 
-type ButtonColor = 'default' | 'white'
+type ButtonColor = "default" | "white";
 
-type ButtonVariant = 'solid' | 'outlined' | 'ghost' | 'soft'
+type ButtonVariant = "solid" | "outlined" | "ghost" | "soft";
 
 interface ElementProps {
-  button: React.ButtonHTMLAttributes<HTMLButtonElement>
-  a: React.AnchorHTMLAttributes<HTMLAnchorElement>
+  button: React.ButtonHTMLAttributes<HTMLButtonElement>;
+  a: React.AnchorHTMLAttributes<HTMLAnchorElement>;
 }
 
-type Element = keyof ElementProps
+type Element = keyof ElementProps;
 
 interface ButtonProps<E extends Element> {
-  as?: E
-  size?: ButtonSize
-  variant?: ButtonVariant
-  color?: ButtonColor
-  pulse?: boolean
-  icon?: JSX.Element
-  loading?: boolean
-  disabled?: boolean
+  as?: E;
+  size?: ButtonSize;
+  variant?: ButtonVariant;
+  color?: ButtonColor;
+  pulse?: boolean;
+  icon?: JSX.Element;
+  loading?: boolean;
+  disabled?: boolean;
 }
 
 export function Button<E extends Element>(
-  props: ButtonProps<E> & ElementProps[E],
+  props: ButtonProps<E> & ElementProps[E]
 ) {
   const {
-    as: Element = 'button',
+    as: Element = "button",
     loading = false,
     children,
-    size = 'medium',
-    variant = 'solid',
-    color = 'default',
+    size = "medium",
+    variant = "solid",
+    color = "default",
     pulse,
     icon,
     disabled,
     className,
     ...rest
-  } = props
+  } = props;
 
   return (
     <Element
-      className={
-        cx(
-          styles.button,
-          styles[`variant-${variant}`],
-          styles[`size-${size}`],
-          styles[`color-${color}`],
-          pulse && styles['pulse'],
-          className,
-        )
-      }
+      className={cx(
+        styles.button,
+        styles[`variant-${variant}`],
+        styles[`size-${size}`],
+        styles[`color-${color}`],
+        pulse && styles["pulse"],
+        className
+      )}
       disabled={disabled || loading}
       {...rest}
     >
@@ -65,35 +63,37 @@ export function Button<E extends Element>(
           <div className={styles.loader} />
         </span>
       ) : icon ? (
-        <span className={styles.icon}>
-          {icon}
-        </span>
+        <span className={styles.icon}>{icon}</span>
       ) : null}
     </Element>
-  )
+  );
 }
 
-export function NavButton(props: ButtonProps<'button'> & Omit<ElementProps['button'], 'onClick'> & {to: string}) {
-  const navigate = useNavigate()
-  return (
-    <Button {...props} onClick={() => navigate(props.to)} />
-  )
+export function NavButton(
+  props: ButtonProps<"button"> &
+    Omit<ElementProps["button"], "onClick"> & { to: string }
+) {
+  const navigate = useNavigate();
+  return <Button {...props} onClick={() => navigate(props.to)} />;
 }
 
-export function CopyButton(props: ButtonProps<'button'> & ElementProps['button'] & {content: string | number}) {
-  const { content, children, onClick, ...rest } = props
-  const [copied, setCopied] = React.useState(false)
+export function CopyButton(
+  props: ButtonProps<"button"> &
+    ElementProps["button"] & { content: string | number }
+) {
+  const { content, children, onClick, ...rest } = props;
+  const [copied, setCopied] = React.useState(false);
 
   const click = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    onClick && onClick(e)
-    await navigator.clipboard.writeText(content)
-    setCopied(true)
-    setTimeout(() => setCopied(false), 500)
-  }
+    onClick && onClick(e);
+    await navigator.clipboard.writeText(content);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 500);
+  };
 
   return (
     <Button {...rest} onClick={click}>
-      {copied ? 'Copied' : children}
+      {copied ? "Copied" : children}
     </Button>
-  )
+  );
 }
